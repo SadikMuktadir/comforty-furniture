173221673ea40fce3278292048c4617b5f42bf42
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2025-12-20 21:15:14 +0600

    redux added to the app

--- providers/StoreProvider.tsx
+++ providers/StoreProvider.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable react-hooks/refs */
-"use client"
+'use client';
 
 import { AppStore, makeStore } from '@/redux/store';
 import { ReactNode, useRef } from 'react';
 import { Provider } from 'react-redux';
 
 export default function StoreProvider({ children }: { children: ReactNode }) {
+  const storeRef = useRef<AppStore>(undefined);
 
-     const storeRef = useRef<AppStore>(undefined)
   if (!storeRef.current) {
-    storeRef.current = makeStore()
+    storeRef.current = makeStore();
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
